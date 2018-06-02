95caa5287422fae4bec794b2bb40bde283e083a2
jojopirker <j.pirker@gmail.com>
2018-06-02 16:18:48 +0200

    updatet demoData.sql now v0.2

--- demodata.sql
+++ demodata.sql
@@ -19,10 +19,27 @@ INSERT INTO KUNDE (KundeID, Name, Mail, Telefon, Strasse, OrtId) VALUES(2, "Mari
 Select * From kunde;
 COMMIT;
 
-# Demo Bestellungen
+# Demo Kundenbestellungen
 INSERT INTO Kundenbestellung (KundenID, KundenbestellungsID) VALUES (2, 1);
 INSERT INTO Kundenbestellung (KundenID, KundenbestellungsID) VALUES (1, 2);
 SELECT * FROM Kundenbestellung;
 COMMIT;
 
-# 
+# Demo Kundenlieferungen
+INSERT INTO Kundenlieferung (KundenlieferungsID, KundenbestellungsID, versanddatum, abgeschlossen) VALUES (1,1, STR_TO_DATE('Wednesday, June 2, 2014', '%W, %M %e, %Y') , FALSE);
+INSERT INTO Kundenlieferung (KundenlieferungsID, KundenbestellungsID, versanddatum, abgeschlossen) VALUES (2,2, CURDATE() , FALSE);
+SELECT * FROM Kundenlieferung;
+COMMIT;
+
+# Demo Lieferantenbestellung
+INSERT INTO Lieferantenbestellung (LieferantenbestellungsID, LieferantID, ZahlungsmethodeID) VALUES (1,1,1);
+INSERT INTO Lieferantenbestellung (LieferantenbestellungsID, LieferantID, ZahlungsmethodeID) VALUES (2,3,3);
+SELECT * FROM Lieferantenbestellung;
+COMMIT;
+
+# Demo Lieferantenlieferung
+INSERT INTO Lieferantenlieferungen (lieferantenlieferungID, Eingangsdatum, LieferbestellungsID) VALUES (1, CURDATE(), 1);
+INSERT INTO Lieferantenlieferungen (lieferantenlieferungID, Eingangsdatum, LieferbestellungsID) VALUES (2, CURDATE()+1, 2);
+SELECT * FROM Lieferantenlieferungen;
+COMMIT;
+

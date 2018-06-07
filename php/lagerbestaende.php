00df0547caad3f4ef75e1d9a30c3228abb56827c
Roman Muehlfeldner <roman@muehlfeldner.net>
2018-06-07 17:01:41 +0200

    Added modal to lagerbestaende.

--- php/lagerbestaende.php
+++ php/lagerbestaende.php
@@ -8,21 +8,33 @@
 include "../models/Artikel.php";
 include "../DB.php";
 $db = New DB();
-$artikelarray = $db->getArtikel();
-$artikelTable = "";
-foreach ($artikelarray as $artikel) {
-    $artikelTable .= "<tr align = \"center\">
+
+if (!empty($_GET['artikelid'])) {
+    $artikelId = $_GET['artikelid'];
+
+
+    $artikel = $db->getArtikelById($artikelId);
+
+    echo json_encode($artikel);
+
+} else {
+//    GET ALL
+    $artikelarray = $db->getArtikel();
+    $artikelTable = "";
+    foreach ($artikelarray as $artikel) {
+        $artikelTable .= "<tr align = \"center\">
                             <td class=\"hidden-xs\">" . $artikel->getArtikelID() . "</td>
                             <td>" . $artikel->getArtikelname() . "</td>
                             <td>" . $artikel->getLagerstand() . "</td>
                             <td align=\"center\">
                               <a class=\"btn btn-default\" 
                               data-toggle=\"modal\" 
-                              data-target=\"#articleModal\" 
+                              data-target=\"#lagerstandModal\" 
                               data-article-id=" . $artikel->getArtikelID() . "
                               data-article-name='" . $artikel->getArtikelname() . "'><em class=\"fa fa-pencil\"></em></a>
                             </td>
                           </tr>";
+    }
+    echo $artikelTable;
 }
-echo $artikelTable;
 ?>
--- static/scripts/lagerbestaende.js
+++ static/scripts/lagerbestaende.js
@@ -1,7 +1,57 @@
 (function () {
 
-    $('#lagerbestaendeliste').load('../php/lagerbestaende.php', () => {
+    // Artikel Tabelle laden.
+    const loadBestaende = () => {
+        $('#lagerbestaendeliste').load('../php/lagerbestaende.php', () => {
+        });
+    };
+
+    loadBestaende();
+
+
+    const modal = $('#lagerstandModal');
+
+    let articleModalInstanz = null;
+
+    modal.on('show.bs.modal', function (event) {
+        const button = $(event.relatedTarget); // Button that triggered the modal
+        const articleId = button.data('article-id'); // Extract info from data-* attributes
+        const articleName = button.data('article-name');
+
+        articleModalInstanz = new LagerbestandModal(articleId);
+
+        const modal = $(this);
+        modal.find('.modal-title').text('Artikel ' + articleName);
     });
+
+    modal.on('hidden.bs.modal', (event) => {
+        loadBestaende();
+    });
+
+
+    class LagerbestandModal {
+
+        constructor(artikelid) {
+
+            this.id = $('#artikelid');
+            this.name = $('#artikelname');
+            this.bestand = $('#bestand');
+
+            $.ajax({
+                url: '../php/lagerbestaende.php?artikelid=' + artikelid
+            })
+                .done((data) => {
+                    const artikel = JSON.parse(data);
+                    this.setValues(artikel);
+                });
+        }
+
+        setValues(artikel) {
+            this.id.val(artikel.artikelID);
+            this.name.val(artikel.artikelname);
+            this.bestand.val(artikel.lagerstand);
+        }
+    }
 
 }());
 

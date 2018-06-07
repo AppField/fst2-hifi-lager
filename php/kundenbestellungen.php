2692acac97ac5e6c86fb0e58171a4380d0752445
jojopirker <j.pirker@gmail.com>
2018-06-07 13:03:49 +0200

    new index.html alle html in static außer index php ordner hinzugefügt

---
+++ php/kundenbestellungen.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: johannes
+ * Date: 07.06.18
+ * Time: 12:54
+ */
+include "../models/Bestellung.php";
+include "../models/Kundenbestellung.php";
+include "DB.php";
+$db = New DB();
+$kundenbestellungen = $db->getKundenbestellungen();
+$bestellungstable = "";
+foreach ($kundenbestellungen as $bestellung) {
+    $bestellungstable .= "<tr align = \"center\">
+                            <td class=\"hidden-xs\">" . $bestellung->getBestellungsID() . "</td>
+                            <td>Offen</td>
+                            <td align=\"center\">
+                                <a class=\"\" href=\"bestelldetails.php?id=" . $bestellung->getBestellungsID() . "\">
+                                    <button class=\"btn fa fa-edit\" ></button>
+                                </a>
+                            </td>
+                          </tr>";
+}
+echo $bestellungstable;

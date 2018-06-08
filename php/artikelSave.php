78c580f792199896c34e02ab9efefe6e8bf8cb91
jojopirker <j.pirker@gmail.com>
2018-06-08 12:20:19 +0200

    added commit

--- php/artikelSave.php
+++ php/artikelSave.php
@@ -15,7 +15,7 @@ if(isset($_POST['artikelid'])){
         }
     }
     if(isset($_POST['lagerort'])){
-        if(!$db->updateArtikelName($_POST['artikelid'], $_POST['lagerort'])){
+        if(!$db->updateArtikelLagerort($_POST['artikelid'], $_POST['lagerort'])){
             $body .= "Error updating Artikel Lagerort";
         }
     }

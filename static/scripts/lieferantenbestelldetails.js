dbf612e0a55b9650255e960d9e1eb29317559644
Roman Muehlfeldner <roman@muehlfeldner.net>
2018-06-08 11:19:16 +0200

    refactored lieferantenbestelldetails.js

--- static/scripts/lieferantenbestelldetails.js
+++ static/scripts/lieferantenbestelldetails.js
@@ -1,18 +1,36 @@
-function getQueryVariable(variable)
-    {
-        var query = window.location.search.substring(1);
-        var vars = query.split("&");
-        for (var i=0;i<vars.length;i++) {
-            var pair = vars[i].split("=");
-            if(pair[0] == variable){return pair[1];}
+(function () {
+    const lieferantInput = $('#lieferant');
+    const artikelTable = $('#artikelTable');
+    const lieferungenTable = $('#lieferungen');
+
+
+    function getQueryVariable(variable) {
+        const query = window.location.search.substring(1);
+        const vars = query.split("&");
+        for (let i = 0; i < vars.length; i++) {
+            const pair = vars[i].split("=");
+            if (pair[0] === variable) {
+                return pair[1];
+            }
         }
-        return(false);
+        return (false);
     }
 
     $('#bestellID').val(getQueryVariable('id'));
-    $('#lieferant').valueOf().load('../php/lieferantenbestelldetails/getLieferant.php?id='+getQueryVariable('id'), () => {
-});
-    $('#artikelTable').load('../php/lieferantenbestelldetails/artikelTable.php?id='+getQueryVariable('id'), () => {
-});
-    $('#lieferungen').load('../php/lieferantenbestelldetails/getLieferungen.php?id='+getQueryVariable('id'), () => {
-});
+
+    $.ajax({
+        url: '../php/lieferantenbestelldetails/getLieferant.php?id=' + getQueryVariable('id')
+    }).done(data => {
+        lieferantInput.val(data);
+    });
+
+
+    artikelTable.load('../php/lieferantenbestelldetails/artikelTable.php?id=' + getQueryVariable('id'), () => {
+
+    });
+
+    lieferungenTable.load('../php/lieferantenbestelldetails/getLieferungen.php?id=' + getQueryVariable('id'), () => {
+
+    });
+
+})();

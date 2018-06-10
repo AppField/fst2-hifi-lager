27bb63d53ac2ba4ab8a7a9ea00e632df63e289fa
an0101st <a.stanusic.as@gmail.com>
2018-06-10 22:35:12 +0200

    suchfunktionen

--- static/scripts/lieferscheine.js
+++ static/scripts/lieferscheine.js
@@ -3,3 +3,21 @@
     $('#lieferscheinliste').load('../php/lieferscheine.php', () => {
     });
 }());
+
+function mySearch() {
+    var input, filter, table, tr, td, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[1];
+        if (td) {
+            if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}

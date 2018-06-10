b43d26f9a3a3f4aca11e32afbecfb4dd9fe3915b
jojopirker <j.pirker@gmail.com>
2018-06-10 13:50:19 +0200

    hoffentlich jetzt ein richtiges sql statement + neues model für offenee artikel

--- php/kundenbestelldetails/getOffeneArtikel.php
+++ php/kundenbestelldetails/getOffeneArtikel.php
@@ -17,12 +17,7 @@ $body = "";
 if (isset($_GET["id"])) {
     $Lieferantenartiekl = $db->getOffeneArtikelLieferantenbestellung($_GET["id"]);
     foreach ($Lieferantenartiekl as $artikel){
-        $body .= "<tr align = \"center\">
-                            <td class=\"hidden-xs\">".$artikel->getID()."</td>
-                            <td>".$artikel->getBezeichnung()."</td>
-                            <td>".$artikel->getAnzahl()."</td>
-                          </tr>";
-        $template = '<li class="list-group-item d-flex justify-content-between align-items-center"
+        $template .= '<li class="list-group-item d-flex justify-content-between align-items-center"
                 draggable="true" data-artikel-id="'.$artikel->getID().'" data-artikel-name="'.$artikel->getBezeichnung().'"
                 data-artikel-anzahl="'.$artikel->getAnzahl().'">
                     '.$artikel->getBezeichnung().'
@@ -34,4 +29,4 @@ if (isset($_GET["id"])) {
 
 
 
-echo $body;
+echo $template;

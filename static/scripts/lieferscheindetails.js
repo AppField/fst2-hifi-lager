83674e1ff6d3614b93f35de0abeab5603a7a6a35
DESKTOP-MH1DQJ1\Diana Öller <dioeller@gmail.com>
2018-06-10 22:16:26 +0200

    lieferscheindetails.js

--- static/scripts/lieferscheindetails.js
+++ static/scripts/lieferscheindetails.js
@@ -1,10 +1,21 @@
 (function () {
 
+    function getQueryVariable(variable) {
+        const query = window.location.search.substring(1);
+        const vars = query.split("&");
+        for (let i = 0; i < vars.length; i++) {
+            const pair = vars[i].split("=");
+            if (pair[0] === variable) {
+                return pair[1];
+            }
+        }
+        return (false);
+    }
 
-    $('#Kundendaten').load('../php/lieferscheindetails/getKundendaten.php', () => {
+    $('#Kundendaten').load('../php/lieferscheindetails/getKundendaten.php?id='+ getQueryVariable('id'), () => {
     });
 
-    $('#LieferscheinArtikelTable').load('../php/lieferscheindetails/getArtikelTableLieferschein.php', () => {
+    $('#LieferscheinArtikelTable').load('../php/lieferscheindetails/getArtikelTableLieferschein.php?id='+ getQueryVariable('id'), () => {
     });
 
 }());

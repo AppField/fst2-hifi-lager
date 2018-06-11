28f0096640b2218f49a57d7320b6be96083525ea
an0101st <a.stanusic.as@gmail.com>
2018-06-11 15:20:35 +0200

    db anpassung

--- DB.php
+++ DB.php
@@ -417,17 +417,6 @@ class DB{
         $this->dbobject->query("commit");
     }
 
-    function createlagerlogTest(){
-        $time = "CURRENT_TIMESTAMP";
-//        $query = "INSERT INTO Lagerlog Values (".$aid.", ".$korrektur.", ".$anzahl.", ".$time.", 000");";
-        $query = "INSERT INTO Lagerlog (`ArtikelID`,`Aenderung`,`Anzahl`,`Datum`,`LieferungsID`)
-                  VALUES ('1', 'KA', '5', $time, '000');";
-        $this->doConnect();
-        $this->dbobject->query("SET NAMES 'utf8'");
-        $this->dbobject->query($query);
-        $this->dbobject->query("commit");
-    }
-
 /*    function updateLagerstand($id, $lagerstand){
         $this->doConnect();
         $this->dbobject->query("SET NAMES 'utf8'");

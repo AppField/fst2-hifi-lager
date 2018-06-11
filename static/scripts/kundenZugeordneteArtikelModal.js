9bc86d49f0e4546d00dc424e1ad34de8ecfe1760
Roman Muehlfeldner <roman@muehlfeldner.net>
2018-06-11 23:16:18 +0200

    implemented modal  to show artikel for kundenlieferung.

---
+++ static/scripts/kundenZugeordneteArtikelModal.js
@@ -0,0 +1,28 @@
+(function () {
+
+    const modal = $('#modalZugeordneteLieferungArtikel');
+
+    let modalInstanz = null;
+
+    modal.on('show.bs.modal', function (event) {
+        const button = $(event.relatedTarget); // Button that triggered the modal
+        const lieferungId = button.data('lieferung-id'); // Extract info from data-* attributes
+        const lieferungDatum = button.data('lieferung-datum');
+
+        // modalInstanz = new ZugeordneteArtikelModal(lieferungId);
+
+        const modal = $(this);
+        modal.find('.modal-title').text('Lieferung ' + lieferungId + ' am ' + lieferungDatum);
+        modal.find('#zugeordneteArtikelLieferungTable').load('../php/kundenbestelldetails/getAssignedArtikel.php?id=' + lieferungId);
+    });
+
+    //
+    // class ZugeordneteArtikelModal {
+    //
+    //     constructor(lieferungId) {
+    //         this.modal
+    //
+    //     }
+    // }
+
+}());

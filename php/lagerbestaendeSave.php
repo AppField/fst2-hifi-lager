b04a9e818a3d2e8baa17bde30845f44e8506dea4
an0101st <a.stanusic.as@gmail.com>
2018-06-12 19:26:53 +0200

    fehlermeldung nach erfolgreichen buchen behoben

--- php/lagerbestaendeSave.php
+++ php/lagerbestaendeSave.php
@@ -13,21 +13,19 @@
 //}
 
 include "../DB.php";
-$body = "true";
+$body = "false";
 $db = new DB();
 if(isset($_POST['artikelid'])){
     if(isset($_POST['korrektur'])) {
         if (isset($_POST['anzahl'])) {
-            if (!$db->createlagerlog((string)$_POST['artikelid'], (string)$_POST['anzahl'], (string)$_POST['korrektur'])) {
-                if ($body == "true") $body = "";
-                $body .= "Error updating Bestand";
-                $body .= "<br>";
-                $body .= "ID: " .$_POST['artikelid'];
-                $body .= "<br>";
-                $body .= "Korrektur: " .$_POST['korrektur'];
-                $body .= "<br>";
-                $body .= "Anzahl: " .$_POST['anzahl'];
-            }
+//            if (!$db->createlagerlog((string)$_POST['artikelid'], (string)$_POST['anzahl'], (string)$_POST['korrektur'])) {
+//                echo "error beim artikeleingang erstellen";
+//                return;
+//            }
+            $do = !$db->createlagerlog((string)$_POST['artikelid'], (string)$_POST['anzahl'], (string)$_POST['korrektur']);
+
+            if($do == false){ echo "error beim erstellen der Lieferung"; return;}
+            $body = "true";
         }
     }
 }

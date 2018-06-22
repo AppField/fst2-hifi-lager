73926cbf0d8f38c2c28056a6ade396a1dadd23b7
jojopirker <j.pirker@gmail.com>
2018-06-22 13:22:34 +0200

    getOffeneBestellteArtikel

--- DB.php
+++ DB.php
@@ -298,6 +298,23 @@ class DB{
         return $artikel;
     }
 
+    function getOffeneBestellteArtikel($id){
+        //$this->doConnect();
+        $this->dbobject->query("SET NAMES 'utf8'");
+        $artikel = array();
+        $result = $this->dbobject->query("SELECT ArtikelID, Artikelname, (Bestellt-IFNULL(Eingegangen,0)) As Offen FROM (SELECT * FROM (SELECT Anzahl as Bestellt, ArtikelID 
+        FROM Lieferantenartikel WHERE ArtikelID = ".$id.") as Bestellung Left JOIN
+        (SELECT SUM(Anzahl) as Eingegangen, Artikel_ArtikelID as ArtikelID FROM Artikeleingang 
+        JOIN Lieferantenlieferungen USING(LieferantenLieferungID) WHERE ArtikelID = ".$id." GROUP BY Artikel_ArtikelID) as Lieferung
+        USING (ArtikelID)) as Results JOIN Artikel USING(ArtikelID) WHERE Eingegangen is null OR Eingegangen < Bestellt");
+        while ($row = $result->fetch_object()) {
+            $offener = new OffenerArtikel($row->ArtikelID, $row->Artikelname, $row->Offen);
+            array_push($artikel, $offener);
+        }
+        //$this->close();
+        return $artikel;
+    }
+
     function getOffeneArtikelKundenbestellung($id){
         //$this->doConnect();
         $this->dbobject->query("SET NAMES 'utf8'");
